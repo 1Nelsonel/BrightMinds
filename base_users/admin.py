6f5a544bdac7c9f193058d9211ccd0012e5290d4
Nelson Mokoro <nelsonmokoro37@gmail.com>
2023-04-18 13:28:07 +0300

    success

--- base_users/admin.py
+++ base_users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from .models import UserProfile
+from .models import UserProfile,Payment
 
 
 # Define an inline for UserProfile to be included in the UserAdmin
@@ -26,3 +26,10 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'mobile_number', 'id_number')
     list_filter = ('user__is_superuser',)
 
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('transaction_id', 'amount', 'phone_number', 'reference', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('transaction_id', 'phone_number', 'reference')
+    readonly_fields = ('transaction_id', 'amount', 'phone_number', 'reference', 'status', 'status_description', 'created_at')
--- base_users/models.py
+++ base_users/models.py
@@ -20,3 +20,29 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
+
+class Payment(models.Model):
+    # Payment status choices
+    PENDING = 'pending'
+    SUCCESS = 'success'
+    FAILED = 'failed'
+    STATUS_CHOICES = [
+        (PENDING, 'Pending'),
+        (SUCCESS, 'Success'),
+        (FAILED, 'Failed'),
+    ]
+
+    # Payment details
+    transaction_id = models.CharField(max_length=50, unique=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    phone_number = models.CharField(max_length=20)
+    reference = models.CharField(max_length=50)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=PENDING)
+    status_description = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.transaction_id} ({self.amount})'
